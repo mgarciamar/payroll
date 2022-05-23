9ee280f75b30410ec48ec1a76552780255a242c6
Mónica García <monica88sm@gmail.com>
2022-05-23 15:22:45 +0200

    Aplicacion REST

---
+++ src/main/java/payroll/EmployeeRepository.java
@@ -0,0 +1,17 @@
+package payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/*
+By simply declaring the following EmployeeRepository interface we automatically will be able to
+Create new Employees
+Update existing ones
+Delete Employees
+Find Employees (one, all, or search by simple or complex properties)
+
+To get all this free functionality, all we had to do was declare an interface which
+ extends Spring Data JPA’s JpaRepository, specifying the domain type as Employee and the id type as Long.
+ */
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
